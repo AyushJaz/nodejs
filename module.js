b3c2baf682c7903db181635dcbefd4f441692b1b
AyushJaz <ajayush2004@gmail.com>
2024-05-03 14:57:02 +0530

    first module done

---
+++ module.js
@@ -0,0 +1,9 @@
+// in real life projects we nead to work with numerous function
+
+
+// resulting in spegiti codes and making it very dificult to understand to solve this we use module to divide the entire function in multiple file grouping similar functions with each other 
+
+// in this we are creating a calculate libarary to use as a module 
+
+const calculate=require('./calculate.js')
+console.log(calculate);
